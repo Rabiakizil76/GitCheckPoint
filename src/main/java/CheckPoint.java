37e1b61a10376bc052e5c6c2f7dbd6649cb274cf
Rabia <rabiakizil@yahoo.com>
2022-02-16 16:04:17 +0100

    init

---
+++ src/main/java/CheckPoint.java
@@ -0,0 +1,9 @@
+public class CheckPoint {
+
+    public static void main(String[] args) {
+        System.out.println("This is the Git Checkpoint!");
+
+
+    }
+
+}
---
+++ src/main/java/Class1.java
@@ -0,0 +1,21 @@
+public class Class1 {
+    private String name;
+
+    public Class1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String addName(){
+      return name;
+    }
+
+
+        }
